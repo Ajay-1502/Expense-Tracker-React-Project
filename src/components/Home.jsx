7b8ca86d82b57c7826a4a37e7823884b9ed4087a
Ajay-1502 <ahdesai40@gmail.com>
2025-07-26 17:57:03 +0530

    Feature : Logout functionality

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,24 +1,38 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 import './Home.css';
 
 const Home = () => {
+  const history = useHistory();
+
+  const logoutHandler = () => {
+    history.push('/');
+    localStorage.removeItem('token');
+  };
+
   return (
-    <div className="homepage-container">
-      <div className="banner">
-        <p>
-          <span className="incomplete">You can update your profile.</span>
-          <Link to="/complete-profile" className="complete-now">
-            Update My Profile
-          </Link>
+    <>
+      <div className="logout-container">
+        <button className="btn btn-danger" onClick={logoutHandler}>
+          Logout
+        </button>
+      </div>
+      <div className="homepage-container">
+        <div className="banner">
+          <p>
+            <span className="incomplete">You can update your profile.</span>
+            <Link to="/complete-profile" className="complete-now">
+              Update My Profile
+            </Link>
+          </p>
+        </div>
+        <h1 className="welcome-text">
+          👋 Welcome to <span className="brand-name">Expense Tracker</span> 💰
+        </h1>
+        <p className="subtitle">
+          Track, manage, and control your expenses like a pro!
         </p>
       </div>
-      <h1 className="welcome-text">
-        👋 Welcome to <span className="brand-name">Expense Tracker</span> 💰
-      </h1>
-      <p className="subtitle">
-        Track, manage, and control your expenses like a pro!
-      </p>
-    </div>
+    </>
   );
 };
 
